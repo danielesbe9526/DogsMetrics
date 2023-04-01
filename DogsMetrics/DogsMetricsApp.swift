c4b91b3ae208bbf3e6a491229b57f4e845aef67b
Daniel Beltran <daniel.beltran@cornershopapp.com>
2023-04-01 16:50:59 -0500

    Initial Commit

---
+++ DogsMetrics/DogsMetricsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogsMetricsApp.swift
+//  DogsMetrics
+//
+//  Created by Daniel Beltran on 1/04/23.
+//
+
+import SwiftUI
+
+@main
+struct DogsMetricsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
